36e60baedabbd9161b749aeb0197531d4d782522
Bai <1242191782@qq.com>
2026-03-04 17:34:38 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,26 +1,27 @@
+
 # -*- coding: utf-8 -*-
+
+# 文档语言，支持en和zh_CN
+# language = 'en'
 
 import sys
 from pathlib import Path
 
 sys.path.append(str(Path('../').resolve()))
 
-
-# 文档语言，支持en和zh_CN
-# language = 'en'
 language = 'zh_CN'
 
 # 主标题和项目名，必填，显示在html左上角，PDF文件名，PDF封面第一行以及内容页左上角，默认为'奕行智能项目文档模板'
-project = u'龙芯实验室文档'
+project = u'FreeRTOS移植'
 
 # 子标题，显示在PDF封面第二行，不显示子标题则设置为''或注释掉
-subtitle = u'2K1000LA开发板SSD和GMAC驱动使用说明'
+subtitle = u''
 
 # 文档作者，默认为 '奕行智能科技有限公司'，默认根据中英文自动切换，显示在PDF封面以及内容页左下角
-author = u'龙芯实验室'
+author = u'白子尧'
 
 # 版权信息，默认为 '2022-2024, 奕行智能科技有限公司'，根据中英文自动切换
-copyright = u'2022-2024, 龙芯中科技术有限公司'
+copyright = u'2022-2026, 龙芯中科技术有限公司'
 
 # 标记文档状态为Draft还是Release，默认Release，状态显示在PDF封面右下角以及内容页右下角，word需要手动填写
 draft_or_release = u'Release'
@@ -32,9 +33,9 @@ release = 'V1.0'
 version = 'V1.0'
 
 # 是否显示PDF水印，默认True，不显示则设置为False
-pdf_watermark = False
+pdf_watermark = True
 
-# PDF水印文本，默认为'Loongson Intelligence Confidential'
+# PDF水印文本，默认为'EVAS Intelligence Confidential'
 watermarktext = u'Loongson Laboratory Confidential'
 
 # 是否在html右上角显示源代码，默认False，默认True
@@ -51,4 +52,3 @@ extensions = ['sphinx_loongsonlab_theme']
 
 # Sphinx主题，必填，不要修改
 html_theme = 'sphinx_loongsonlab_theme'
-
